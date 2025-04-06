b19eed03a10da8461aa2203925eed4236405b719
cannedoxygen <126140727+cannedoxygen@users.noreply.github.com>
2025-04-06 06:23:34 -0500

    Brief description of your changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV || 'production',
   entry: './bootstrap.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, './'),
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   module: {
     rules: [
@@ -22,10 +23,23 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name][ext]'
+        }
       }
     ]
   },
   resolve: {
     extensions: ['.js', '.jsx']
-  }
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  },
+  devtool: 'source-map'
 };
